2fddcd7d6620c711cf018f8ffc20eb9314235a98
Zoey Farago <zf.zoodle@gmail.com>
2021-03-04 01:52:00 +0000

    Active Record counts for categories and products

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,5 +3,7 @@ class Admin::DashboardController < ApplicationController
   before_filter :authenticate
 
   def show
+    @product = Product.count(:all)
+    @category = Category.count(:all)
   end
 end
