30c28a3ac12fa220f10d73a256d743425a5d237d
Zoey Farago <zf.zoodle@gmail.com>
2021-03-04 07:34:00 +0000

    Implemented ability to add shopping categories, small rendering bug above Category name title

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,10 +1,27 @@
 class Admin::CategoriesController < ApplicationController
   def index
+    @categories = Category.all.order(created_at: :desc)
   end
 
   def new
+    @category = Category.new
   end
 
   def create
+    @category = Category.new(product_params)
+
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'Category created!'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:category).permit(
+      :name
+    )
   end
 end
--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,7 @@
 class CategoriesController < ApplicationController
+  def index
+    @categories = Category.all.order(created_at: :desc)
+  end
 
   def show
     @category = Category.find(params[:id])
