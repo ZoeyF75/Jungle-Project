eafad03e85a07736b57d9ee344df6c4e3f043f02
Zoey Farago <zf.zoodle@gmail.com>
2021-03-04 13:53:44 +0000

    Added new sale page

--- app/controllers/admin/sales_controller.rb
+++ app/controllers/admin/sales_controller.rb
@@ -1,6 +1,34 @@
 class Admin::SalesController < ApplicationController
+
+  before_filter :authenticate
+  
   def index
     @sales = Sale.all
     # render :index
+  end
+  
+  def new
+    @sale = Sale.new
+  end
+
+  def create
+    @sale = Sale.new(sale_params)
+
+    if @sale.save
+      redirect_to [:admin, :sales], notice: 'Sale created!'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def sale_params
+    params.require(:category).permit(
+      :name,
+      :percent_off,
+      :starts_on,
+      :ends_on
+    )
   end
 end
