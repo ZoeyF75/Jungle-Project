a1cd9ae4438f8b43a5820e94bd452f6267129392
Zoey Farago <zf.zoodle@gmail.com>
2021-03-10 23:45:03 +0000

    Fixed email bug:

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
 
   def create
     if user = User.authenticate_with_credentials(params[:email], params[:password])
-      session[:user_id] = user_id
+      session[:user_id] = user.id
       redirect_to '/'
     else
       # failure, render login form
